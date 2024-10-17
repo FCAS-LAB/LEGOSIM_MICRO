9824c3bc9c9c8b43a11de4c45130dd420bf2a4b7
qc <>
2024-10-17 19:49:40 +0800

    add npu

--- benchmark/matmul/matmul.cu
+++ benchmark/matmul/matmul.cu
@@ -17,25 +17,31 @@
  * 由矩阵乘法原理可知，我们可将计算任务划分为4个100*100的矩阵相乘，并将结果相加。
  */
 
-#define Row 100
-#define Col 100
+// #define Row 100
+// #define Col 100
+int Row_A = 20;
+int Col_A = 100;
+int Row_B = 100;
+int Col_B = 100;
 
 /**
  * 矩阵乘法的核心函数，由每个线程都会运行一次本函数，
  * 根据线程编号不同计算出位于结果矩阵不同位置的数据。
  */
 
-__global__ void matrix_mul_gpu(int64_t* M, int64_t* N, int64_t* P, int width) {
-    int sumNum = threadIdx.x + threadIdx.y * 10;
-    int i = threadIdx.x;
-    int j = threadIdx.y;
-    int64_t sum = 0;
-    for (int k = 0; k < width; k++) {
-        int64_t a = M[j * width + k];
-        int64_t b = N[k * width + i];
-        sum += a * b;
+__global__ void matrix_mul_gpu(int64_t *M, int64_t *N, int64_t *P, int64_t widthA, int64_t heightA,
+                               int64_t widthB) {
+    int64_t i = threadIdx.x + blockDim.x * blockIdx.x;
+    int64_t j = threadIdx.y + blockDim.y * blockIdx.y;
+    if (i < widthB && j < heightA) {
+        int64_t sum = 0;
+        for (int64_t k = 0; k < widthA; k++) {
+            int64_t a = M[j * widthA + k];
+            int64_t b = N[k * widthB + i];
+            sum += a * b;
+        }
+        P[j * widthB + i] = sum;
     }
-    P[sumNum] = sum;
 }
 
 int main(int argc, char** argv) {
@@ -44,19 +50,20 @@ int main(int argc, char** argv) {
     int idX = atoi(argv[1]);
     int idY = atoi(argv[2]);
     int64_t *d_dataA, *d_dataB, *d_dataC;
-    cudaMalloc((void**)&d_dataA, sizeof(int64_t) * Row * Col);
-    cudaMalloc((void**)&d_dataB, sizeof(int64_t) * Row * Col);
-    cudaMalloc((void**)&d_dataC, sizeof(int64_t) * Col);
+    cudaMalloc((void**)&d_dataA, sizeof(int64_t) * Row_A * Col_A);
+    cudaMalloc((void**)&d_dataB, sizeof(int64_t) * Row_B * Col_B);
+    cudaMalloc((void**)&d_dataC, sizeof(int64_t) * Col_B * Row_A);
 
-    receiveMessage(idX, idY, 0, 0, d_dataA, sizeof(int64_t) * Row * Col);
-    receiveMessage(idX, idY, 0, 0, d_dataB, sizeof(int64_t) * Row * Col);
+    receiveMessage(idX, idY, 3, 3, d_dataA, sizeof(int64_t) * Row_A * Col_A);
+    receiveMessage(idX, idY, 3, 3, d_dataB, sizeof(int64_t) * Row_B * Col_B);
 
     // calculate
     dim3 threadPerBlock(10, 10);
-    dim3 blockNumber(1);
-    matrix_mul_gpu<<<blockNumber, threadPerBlock>>>(d_dataA, d_dataB, d_dataC, Col);
+    dim3 blockNumber((Col_B + threadPerBlock.x - 1) / threadPerBlock.x,
+                         (Row_A + threadPerBlock.y - 1) / threadPerBlock.y);
+    matrix_mul_gpu<<<blockNumber, threadPerBlock>>>(d_dataA, d_dataB, d_dataC, Col_A, Row_A, Col_B);
 
-    sendMessage(0, 0, idX, idY, d_dataC, 100 * sizeof(int64_t));
+    sendMessage(3, 3, idX, idY, d_dataC, Col_B * Row_A * sizeof(int64_t));
     cudaFree(d_dataA);
     cudaFree(d_dataB);
     cudaFree(d_dataC);
